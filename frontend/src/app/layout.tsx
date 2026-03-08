a62399f24422a023ee0dc02bd2a6d9c65a698d83
João Pedro <joaopedrodacosta2014@hotmail.com>
2026-03-08 18:55:45 -0300

    feat: fix darkmode

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -17,12 +17,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt-BR" suppressHydrationWarning>
+    <html lang="pt-BR" className="dark" suppressHydrationWarning>
       <body className={inter.className}>
         <ThemeProvider
             attribute="class"
             defaultTheme="dark"
-            enableSystem
+            enableSystem={false}
             disableTransitionOnChange
           >
             {children}
